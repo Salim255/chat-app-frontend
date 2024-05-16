33926ce4099e46ce7b77006860da8fe41d172289
Salim <salim.hassan.welleat@gmail.com>
2024-05-16 22:12:29 +0200

    built: search bar component

--- chat/src/app/shared/shared.module.ts
+++ chat/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { TagComponent } from "./native-component/tag/tag.component";
 import { ActionComponent } from "./profile/action/action.component";
 import { IonicModule } from "@ionic/angular";
+import { SearchBarComponent } from "./native-component/search-bar/search-bar.component";
 
 @NgModule({
-  declarations: [TagComponent,ActionComponent],
-  exports: [TagComponent, ActionComponent],
+  declarations: [TagComponent,ActionComponent, SearchBarComponent],
+  exports: [TagComponent, ActionComponent,SearchBarComponent],
   imports: [
     IonicModule
   ]
