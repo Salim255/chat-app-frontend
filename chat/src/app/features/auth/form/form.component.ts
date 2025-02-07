095053eef2287f0bb2065cedc398ae6653c94a8c
Salim <salim.hassan.welleat@gmail.com>
2025-02-07 23:44:59 +0100

    refactored: auth form

--- chat/src/app/features/auth/form/form.component.ts
+++ chat/src/app/features/auth/form/form.component.ts
@@ -92,5 +92,4 @@ export class FormComponent implements OnInit {
     }
   }
 
-
 }
--- chat/src/app/pages/auth/auth.page.ts
+++ chat/src/app/pages/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
 import { Subscription } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Component({
     selector: 'app-auth',
@@ -10,28 +11,10 @@ import { Subscription } from 'rxjs';
     standalone: false
 })
 
-export class AuthPage implements OnInit, OnDestroy {
- autMode: boolean =  true;
-  private authModeSubscription!: Subscription;
-
-  constructor(private authService: AuthService, private router: Router) {}
-
-  switchAuth(event: any){
-    this.autMode =  !this.autMode
+export class AuthPage {
+  constructor( private location: Location) {}
+  customBack(){
+    this.location.back();
   }
 
-  ngOnInit(): void {
-    this.authModeSubscription = this.authService.getAuthMode.subscribe(mode => {
-
-    });
-  }
-
-  ionWillEnter() {
-    this.authModeSubscription = this.authService.getAuthMode.subscribe(mode => {
-      console.log(mode)
-    });
-  }
-  ngOnDestroy(): void {
-    if (this.authModeSubscription) this.authModeSubscription.unsubscribe
-  }
 }
